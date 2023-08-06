288c41dff8272069052173982671ed308930e4e6
IASIKO <iaseshviligi@gmail.com>
2023-08-06 14:04:08 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# food-order-app-master
+
+Welcome to the Food Order App repository! This repository contains the source code for a food ordering application developed by [Giorgi Iaseshvli].
+
+## Introduction
+
+The Food Order App is  an innovative food ordering application built using React.js. Whether you're craving a quick snack or a hearty meal, 
+Food-order-app provides a seamless and user-friendly experience for ordering your favorite dishes. With powerful features like real-time updates, 
+flexible state management, and integration with Firebase APIs, food-order-app transforms your food ordering process into a delightful journey.
+## Features
+
+useContext:
+Efficiently shares data between components without prop drilling.
+
+Firebase API:
+Seamlessly integrates authentication, database, and storage services.
+
+Module.css:
+Locally scopes styles to prevent conflicts and ensure isolation.
+
+useReducer:
+Manages complex state changes with a structured approach.
+
+Validation for Forms:
+Ensures user input adheres to specified criteria, reducing errors.
+
+
+## Installation
+
+Provide a step-by-step guide on how to install and set up your application. Include any dependencies that need to be installed and configuration steps if necessary.
+
+1. Clone the repository: `git clone https://github.com/IASIKO/food-order-app-master.git`
+2. Navigate to the project directory: `cd food-order-app-master`
+3. npm: A package manager for installing and managing JavaScript packages and dependencies.
+
+## Usage
+
+Explain how to use your application. Provide clear instructions and examples of different use cases. 
+
+1. Open a terminal and navigate to the project directory.
+2. Run the command: `npm start`
+3. Open a web browser and go to `http://localhost:3000` to access the app.
+
+
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,86 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [isError, setIsError] = useState(null)
+
+  const fetchMeals = async () => {
+    setIsloading(true);
+    setIsError(null)
+    try {
+      const response = await fetch(
+        "https://custom-hooks-32624-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!")
+      }
+      const data = await response.json();
+
+      const mealsData = [];
+
+      for (const key in data) {
+        mealsData.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(mealsData);
+      setIsloading(false);
+    } catch (error) {
+      setIsError(error.message)
+    }
+  };
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  let content = <p>not found</p>;
+
+  if (meals.length > 0) {
+    content = mealsList;
+  }
+
+  if (isLoading) {
+    content = (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <section className={classes.MealsError}>
+        <p>{isError}</p>
+      </section>
+    );
+  }
+
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{content}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
